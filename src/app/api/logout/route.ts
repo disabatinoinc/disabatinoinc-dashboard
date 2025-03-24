0c0d54384c21e902208ca299481698c59a8d1f47
Mike Disabatino <mdisabatino@DISABATINO.local>
2025-03-24 11:01:26 -0400

    retesting auth

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -2,8 +2,17 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
-    const baseUrl = req.nextUrl.origin
-    const res = NextResponse.redirect(`${baseUrl}/`)
-    res.cookies.set('auth_token', '', { maxAge: 0 })
+    const fallbackHost = process.env.NEXT_PUBLIC_FALLBACK_HOST
+    const referer = req.headers.get('referer')
+    const redirectTo = referer || fallbackHost
+    const res = NextResponse.redirect(`${redirectTo}/`)
+    res.cookies.set('auth_token', '', {
+        maxAge: 0,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        path: '/',
+    })
+
     return res
 }
