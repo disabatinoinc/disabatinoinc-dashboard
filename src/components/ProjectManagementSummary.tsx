05b635768f6c067aaf14ebc4a171388824ea2c40
Mike Disabatino <mdisabatino@DISABATINO.local>
2025-04-24 10:14:28 -0400

    added base page for project management

---
+++ src/components/ProjectManagementSummary.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const ProjectManagementSummary = () => {
+    return (
+        <Box>
+            <Typography variant="h4" sx={{ color: "white" }}>Project Management Summary</Typography>
+            <Typography>
+                Buildertrend Projects incoming...
+            </Typography>
+        </Box>
+    );
+};
+
+export default ProjectManagementSummary;
