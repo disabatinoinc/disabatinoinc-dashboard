3d90961c0eee8dee87d6a881d2f4ba6d0bc14bac
Mike Disabatino <mdisabatino@DISABATINO.local>
2025-06-05 09:22:36 -0400

    fixed linting

--- src/components/collections/CollectionsDetails.tsx
+++ src/components/collections/CollectionsDetails.tsx
@@ -95,18 +95,24 @@ const CollectionsDetails = () => {
 
 
     const sortedData = [...projects].sort((a, b) => {
-        let aValue: any = a[orderBy as keyof CollectionSummary];
-        let bValue: any = b[orderBy as keyof CollectionSummary];
+        let aValue: string | number | boolean | null = null;
+        let bValue: string | number | boolean | null = null;
 
         if (orderBy === "nextBillingMilestone") {
+            aValue = a.nextBillingMilestone ?? "";
+            bValue = b.nextBillingMilestone ?? "";
+
             return order === "asc"
-                ? milestoneOrder.indexOf(aValue || "") - milestoneOrder.indexOf(bValue || "")
-                : milestoneOrder.indexOf(bValue || "") - milestoneOrder.indexOf(aValue || "");
+                ? milestoneOrder.indexOf(aValue) - milestoneOrder.indexOf(bValue)
+                : milestoneOrder.indexOf(bValue) - milestoneOrder.indexOf(aValue);
         }
 
         if (orderBy === "dynamicDate") {
             aValue = getDynamicDate(a) ? new Date(getDynamicDate(a)!).getTime() : -Infinity;
             bValue = getDynamicDate(b) ? new Date(getDynamicDate(b)!).getTime() : -Infinity;
+        } else {
+            aValue = a[orderBy] ?? "";
+            bValue = b[orderBy] ?? "";
         }
 
         if (aValue < bValue) return order === "asc" ? -1 : 1;
